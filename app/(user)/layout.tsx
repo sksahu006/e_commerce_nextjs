aec22ba025ce411ee5aef1b4971f01bedf8dc07d
Sumanta <kumarsumanta583@gmail.com>
2024-11-08 08:16:49 +0530

    navbar fixed

---
+++ app/(user)/layout.tsx
@@ -0,0 +1,27 @@
+import Footer from "@/components/Footer";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import Navbar from "@/components/Navbar";
+import { headers } from "next/headers";
+import { Suspense } from "react";
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const headersList = headers();
+  const referer = headersList.get("referer");
+
+  // If referer is not available, assume root path
+  const url = referer ? new URL(referer) : { pathname: "/" };
+  const pathname = url.pathname || "/";
+  return (
+    <div className="h-screen bg-gray-100">
+      <Navbar pathname={pathname} />
+      <Suspense fallback={<LoadingSpinner />}>
+        <main className="flex-1 overflow-y-auto p-5">{children}</main>
+      </Suspense>
+      <Footer />
+    </div>
+  );
+}
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,7 +2,8 @@ import CategorySection from "@/components/CategorySection";
 import FeaturedProducts from "@/components/Featuredproduct";
 import NewArrivals from "@/components/NewArrivals";
 import SliderHome from "@/components/SliderHome";
-import Marquee from 'react-fast-marquee';
+import Marquee from "react-fast-marquee";
+
 export default function Home() {
   return (
     <div className="mt-20 flex flex-col gap-3">
@@ -12,24 +13,24 @@ export default function Home() {
       <section>
         <NewArrivals />
       </section>
-      <div className=' bg-[#ffc719] p-2 '>
-
-                <Marquee speed={50} gradient={false} >
-                    {[...Array(10)].map((_, index) => (
-                        <div key={index} className='font-thunder-lc text-3xl ml-3 mr-3 uppercase'>
-                            the town troops
-                        </div>
-                    ))}
-                </Marquee>
+      <div className=" bg-[#ffc719] p-2 ">
+        <Marquee speed={50} gradient={false}>
+          {[...Array(10)].map((_, index) => (
+            <div
+              key={index}
+              className="font-thunder-lc text-3xl ml-3 mr-3 uppercase"
+            >
+              the town troops
             </div>
+          ))}
+        </Marquee>
+      </div>
       <section>
         <FeaturedProducts />
       </section>
       <section>
-        <CategorySection/>
+        <CategorySection />
       </section>
-
-
     </div>
   );
 }
